8db446414401da0b274335c0edd3929ec8f862d8
MerlinB <merlin.buczek@gmail.com>
2017-06-20 16:15:51 +0200

    Forms

--- src/projekte/forms.py
+++ src/projekte/forms.py
@@ -6,13 +6,13 @@ from django.db.models.signals import pre_save
 class StufenModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return obj.betrag
-        
+
         # def __init__(self, projekt, *args, **kwargs):
         #     self.fields['Stufe'].queryset = Stufen.objects.filter(projekt = projekt)
 
 class SpendenModelForm(forms.ModelForm):
-    Stufe = StufenModelChoiceField(empty_label = None)
-    
+    Stufe = StufenModelChoiceField(empty_label = None, queryset = None)
+
     class Meta():
         model = Spenden
         fields = [
@@ -21,18 +21,18 @@ class SpendenModelForm(forms.ModelForm):
             "Email",
             "Stufe"
         ]
-        
+
     def __init__(self, projekt, *args, **kwargs):
         super(SpendenModelForm, self).__init__(**kwargs)
         self.fields['Stufe'].queryset = Stufen.objects.filter(projekt = projekt)
         # self.fields['Stufe'].empty_label = None
         # self.fields['Stufe'].to_field_name = "betrag"
         print(projekt)
-        
+
 
 def pre_save_receiver(sender, instance, *args, **kwargs):
     instance.Validiert = False
-    # instance.Stufe = 
+    # instance.Stufe =
 
 
 pre_save.connect(pre_save_receiver, sender=Spenden)
@@ -41,6 +41,6 @@ pre_save.connect(pre_save_receiver, sender=Spenden)
 
 #class StufenForm(forms.Form):
 #    Stufe = forms.ModelChoiceField(queryset=Stufen.objects.all())
-    
+
 # WIE EIN FORM AUS DATENBANKEINTRÄGEN ERZEUGEN?
 # WIE BEIDE VERBINDEN?
