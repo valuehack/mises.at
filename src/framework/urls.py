16905ee03d70de66ff8c1bde6799af6e52532ff9
Merlin Buczek <merlin.buczek@gmail.com>
2018-05-07 10:07:27 +0200

    google site verification

--- src/framework/urls.py
+++ src/framework/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from .views import home_view
+from django.http import HttpResponse
 
 app_name = 'framework'
 
 urlpatterns = [
     url(r'^$', home_view, name='home'),
+    url(r'^googlee50af1936941f141.html$', lambda request: HttpResponse('google-site-verification: googlee50af1936941f141.html'))
 ]
